4aaaa20b26bf1b821b91608bf252f1457cbbbd14
daiki-kanai <knidik3@gmail.com>
2020-05-11 22:19:26 +0900

    [WIP]createIdentificationView

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
+  devise_scope :user do
+    get 'identification', to: 'users/registrations#new_identification'
+    post 'identifications', to: 'users/registrations#create_identification'
+  end
+  
   root 'goods#index'
   resources :goods
   resources :users, only: :show
