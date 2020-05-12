d4907b2dd7bc79095143c8cc80ea72e27f2c22bf
RYgithub1 <ryo.rec.otani@gmail.com>
2020-05-12 11:21:18 +0900

    テキスト反映、画像反映、編集削除ボタン

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -17,12 +17,28 @@ class GoodsController < ApplicationController
     end
   end
 
+
   def show
+    @good = Good.find_by(params[:id])
+    @pictures = Picture.where(id: @good.pictures.ids)
+    @parents = Category.all.order("id ASC").limit(13)
+  end
+  def edit
+    @good = Good.find(params[:id])
+  end
+  def update
+    good = Good.find(params[:id])
+    good.update(good_params)
+  end
+  def destroy
+    good = Good.find(params[:id])
+    good.destroy
   end
 
-  private
 
+  private
   def good_params
     params.require(:good).permit(:goodsName, :explanation, :category_id, :brand, :goodsCondition, :deliveryFee, :prefecture_id, :deliveryDay, :price, pictures_attributes: [:goodsImage]).merge(user_id: current_user.id)
   end
+
 end
