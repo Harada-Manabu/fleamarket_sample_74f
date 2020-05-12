db733618e6e9550fa1aea72c7f25afd1bf198ff5
daiki-kanai <knidik3@gmail.com>
2020-05-12 16:51:07 +0900

    [WIP]canBeMoveDeliveryAddressView

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
   }
   devise_scope :user do
-    get 'identification', to: 'users/registrations#new_identification'
+    get 'identifications', to: 'users/registrations#new_identification'
     post 'identifications', to: 'users/registrations#create_identification'
+    get 'deliveryAddresses', to: 'users/registrations#new_deliveryAddress'
+    post 'deliveryAddresses', to: 'users/registrations#create_deliveryAddress'
   end
-  
+
   root 'goods#index'
   resources :goods
   resources :users, only: :show
