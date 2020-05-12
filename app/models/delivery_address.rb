3b97c14fc0c1b9702d1159f96207dcb4451a7765
daiki-kanai <knidik3@gmail.com>
2020-05-12 12:35:32 +0900

    [WIP]associatedUserAndDeliveryAddress

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -1,2 +1,4 @@
 class DeliveryAddress < ApplicationRecord
+  belongs_to :user, optional: true
+  validates :familyName, :firstName, :familyNameKana, :firstNameKana, :postCode, :prefecture_id, :city, :address, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :goods
   has_one :identification
+  has_one :deliveryAddress
   
   validates :nickname, presence: true
 
