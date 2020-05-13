9f2c14f23595c2e0d7b57d0f75756d7ec805f83c
daiki-kanai <knidik3@gmail.com>
2020-05-13 16:11:53 +0900

    [WIP]createCardsTable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_025909) do
+ActiveRecord::Schema.define(version: 2020_05_13_070058) do
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "categoryName", null: false
@@ -90,6 +99,7 @@ ActiveRecord::Schema.define(version: 2020_05_12_025909) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
   add_foreign_key "delivery_addresses", "users"
   add_foreign_key "goods", "categories"
   add_foreign_key "goods", "users"
