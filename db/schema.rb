216a80bfe764712e1abdd888a8dbe8b8d092bb94
daiki-kanai <knidik3@gmail.com>
2020-05-13 21:24:13 +0900

    fixCardsMigrationFile

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,12 @@
 ActiveRecord::Schema.define(version: 2020_05_13_070058) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "customer_id", null: false
     t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -98,6 +99,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_070058) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
   add_foreign_key "delivery_addresses", "users"
   add_foreign_key "goods", "categories"
   add_foreign_key "goods", "users"
