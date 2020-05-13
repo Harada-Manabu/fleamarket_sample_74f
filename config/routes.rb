91d4a281633da35614b7eed0c3610855be8144c6
Harada Manabu <haradamanabu.jpn@gmail.com>
2020-05-13 15:12:41 +0900

    Add purchases controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'goods#index'
-  resources :goods
+  resources :goods, except: :index do
+    resources :purchases, only: :index
+  end
   resources :users, only: :show
 end
