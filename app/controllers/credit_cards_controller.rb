a015a92f7c65ab156f16dbc335321256c641cd3a
daiki-kanai <knidik3@gmail.com>
2020-05-14 01:08:40 +0900

    getPayjpToken

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -5,8 +5,8 @@ class CreditCardsController < ApplicationController
   end
 
   def create
-    
-    Payjp.api_key = ['PAYJP_PRIVATE_KEY']
+
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     if params['payjp_token'].blank?
       redirect_to action: "new", alert: "クレジットカードが登録できませんでした。"
     else
@@ -18,7 +18,7 @@ class CreditCardsController < ApplicationController
       @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       
       if @card.save
-
+        redirect_to root_path
       else
         redirect_to action: "create"
       end
