5105a3eca87ea7deb394bec8b43ab142ce1c3c41
daiki-kanai <knidik3@gmail.com>
2020-05-15 00:24:34 +0900

    setGoodsFirstPicture

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,10 @@
 class PurchasesController < ApplicationController
 
+  require "payjp"
+
   def index
     @good = Good.find_by(params[:id])
+    @pictures = Picture.where(id: @good.pictures.ids)
   end
 
 end
