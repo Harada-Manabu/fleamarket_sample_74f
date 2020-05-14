7beddb2566d911eed9bd90cfeb4a4acfabdc30af
daiki-kanai <knidik3@gmail.com>
2020-05-15 01:25:22 +0900

    setdDeliveryAddress

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,6 +5,7 @@ class PurchasesController < ApplicationController
   def index
     @good = Good.find_by(params[:id])
     @picture = Picture.find_by(id: @good.pictures.ids)
+    @deliveryAddress = DeliveryAddress.find_by(user_id: current_user.id)
   end
 
 end
