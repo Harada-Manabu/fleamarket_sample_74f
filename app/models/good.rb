a7fe4f43665eaec32472772901a2982c85b643c2
daiki-kanai <knidik3@gmail.com>
2020-05-18 18:16:33 +0900

    useActivehashInPurchaseView

--- app/models/good.rb
+++ app/models/good.rb
@@ -2,6 +2,7 @@ class Good < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :pictures
+  has_one :purchase
   accepts_nested_attributes_for :pictures, allow_destroy: true
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
