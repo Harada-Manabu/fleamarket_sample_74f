b9d3df38257520bb279d001c34839061044bc25c
izumirin <mariya_0903@icloud.com>
2020-05-20 14:14:07 +0900

    追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to root_path unless user_signed_in? && current_user.id ==@user.id  
   end
+  def logout
+    @user = User.find_by(params[:user_id])
+  end
+
 end
