7c705ccc04775480ccc4fcbb7c8c2ffaed45ffb7
daiki-kanai <knidik3@gmail.com>
2020-05-20 12:55:41 +0900

    fixRailsHelperUsingCutFactory_bot

--- spec/factories/credit_cards.rb
+++ spec/factories/credit_cards.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   
   factory :credit_card do
-    user_id      {"1"}
+    user_id      {"100"}
     customer_id  {"cus_111111111111111"}
     card_id      {"car_aaaaaaaaaaaaaaa"}
   end
