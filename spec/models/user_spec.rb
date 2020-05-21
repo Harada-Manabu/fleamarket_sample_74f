f86ed04816840814d14a60408830bd6d332a363d
daiki-kanai <knidik3@gmail.com>
2020-05-21 18:27:30 +0900

    userModelPasswordTest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -46,15 +46,30 @@ describe User do
 
     it "メールアドレスに@とドメインがあれば登録できる" do
       user = build(:user, email: "test@test.test")
-      user.valid?
       expect(user).to be_valid
     end
 
     it "パスワードがなければ登録できない" do
-      user = 
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("が入力されていません。")
     end
 
+    it "パスワードが入力されていても確認用が入力されていないと登録できない" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
 
+    it "パスワードが6文字以下だと登録できない" do
+      user = build(:user, password: "111111", password_confirmation: "11111")
+      user.valid?
+      expect(user.errors[:password]).to include("は7文字以上に設定して下さい。")
+    end
 
+    it "パスワードが7文字以上なら登録できる" do
+      user = build(:user, password: "2222222", password_confirmation: "2222222")
+      expect(user).to be_valid
+    end
   end
 end
