858eed65fd4f56bdc2af9dda547b232010fc53ba
daiki-kanai <knidik3@gmail.com>
2020-05-21 15:39:14 +0900

    validateDelibveryAddressesTable

--- app/models/identification.rb
+++ app/models/identification.rb
@@ -2,16 +2,17 @@ class Identification < ApplicationRecord
   belongs_to :user, optional: true
   validates :birthday, presence: true
 
-  validates :familyNameKana, :firstNameKana, presence: true,
-                                              format: {
-                                                with: /\A[ぁ-ん]+\z/,
-                                                message: "は全角ひらがなで入力してください"
-                                              }
 
   validates :familyName, :firstName, presence: true,
                                       format: {
                                       with: /\A[ぁ-んァ-ヶー一-龠]+\z/,
                                       message: "は全角で入力してください"
+                                      }
+
+  validates :familyNameKana, :firstNameKana, presence: true,
+                                      format: {
+                                        with: /\A[ぁ-ん]+\z/,
+                                        message: "は全角ひらがなで入力してください"
                                       }
 
 end
