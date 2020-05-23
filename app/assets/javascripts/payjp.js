b6ac100f0b35a44ac31a215ead7f06fa15770287
Harada Manabu <haradamanabu.jpn@gmail.com>
2020-05-23 23:10:45 +0900

    Fix payjp.js

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,30 +1,28 @@
 window.addEventListener("DOMContentLoaded", function () {
-  if (document.getElementById("payment_card_submit-button") != null) {
-    let submit = document.getElementById("payment_card_submit-button");
 
   if (document.getElementById("payment_card_submit-button") != null) {
     let submit = document.getElementById("payment_card_submit-button");
 
     Payjp.setPublicKey('pk_test_9917b060e7c6cd66112275c7');
 
-      submit.addEventListener('click', function(e){ 
+    submit.addEventListener('click', function (e) {
 
       e.preventDefault();
 
       let card = {
-          number: document.getElementById("payment_card_number").value,
-          cvc: document.getElementById("payment_card_cvc").value,
-          exp_month: document.getElementById("payment_card_month").value,
-          exp_year: document.getElementById("payment_card_year").value
+        number: document.getElementById("payment_card_number").value,
+        cvc: document.getElementById("payment_card_cvc").value,
+        exp_month: document.getElementById("payment_card_month").value,
+        exp_year: document.getElementById("payment_card_year").value
       };
 
-      Payjp.createToken(card, function(status, response) { 
-        if (status === 200) { 
-          
+      Payjp.createToken(card, function (status, response) {
+        if (status === 200) {
+
           $(".number").removeAttr("name");
           $(".cvc").removeAttr("name");
           $(".exp_month").removeAttr("name");
-          $(".exp_year").removeAttr("name"); 
+          $(".exp_year").removeAttr("name");
           $("#charge-form").append(
             $('<input type="hidden" name="payjp_token">').val(response.id)
           );
