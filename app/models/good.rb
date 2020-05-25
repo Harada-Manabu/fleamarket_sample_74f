d0bb68dd6c5f4bcee81934928f7608365b1a3d03
RYgithub1 <ryo.rec.otani@gmail.com>
2020-05-25 18:29:54 +0900

    fixEditNewPreviewDelete

--- app/models/good.rb
+++ app/models/good.rb
@@ -2,9 +2,7 @@ class Good < ApplicationRecord
 
   belongs_to :user
   belongs_to :category
-  has_many :pictures
   has_many :pictures, dependent: :destroy
-
   accepts_nested_attributes_for :pictures, allow_destroy: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -12,4 +10,5 @@ class Good < ApplicationRecord
 
   validates_associated :pictures
   validates :user_id, :goodsName, :explanation, :category_id, :goodsCondition, :deliveryFee, :prefecture_id, :deliveryDay, :price, :pictures, presence: true
+
 end
