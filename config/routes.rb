5bdae908304ce2a0748e38863866758fed145ddf
Harada Manabu <haradamanabu.jpn@gmail.com>
2020-05-26 12:13:33 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
 
   root 'goods#index'
   resources :goods, except: :index do
+    # collection do
+    #   get 'categoryChildren' 
+    #   get 'categoryGrandChildren'
+    # end
     member do
       get 'categoryChildren' 
       get 'categoryGrandChildren'
