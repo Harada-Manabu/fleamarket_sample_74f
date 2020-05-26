b42a38ac6f53df7906b6008bfc424f680b26f972
daiki-kanai <knidik3@gmail.com>
2020-05-26 23:09:26 +0900

    ableToGoodsShowPageWhenSoldout

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,7 +2,7 @@ class PurchasesController < ApplicationController
 
   before_action :login_check
   before_action :own_goods
-  # before_action :sold_out
+  before_action :sold_out
   before_action :set_good, only: [:index, :pay]
   require "payjp"
 
@@ -69,13 +69,13 @@ class PurchasesController < ApplicationController
     end
   end
 
-  # def sold_out
-  #   @good = Good.find(params[:good_id])
-  #   if
-  #     @good.buyer_id.present?
-  #     redirect_to root_path
-  #   end
-  # end
+  def sold_out
+    @good = Good.find(params[:good_id])
+    if
+      @good.buyer_id.present?
+      redirect_to root_path
+    end
+  end
 
 
 
